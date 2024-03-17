2068b69737993f6ed7c6787e0a6ebafb7b7370d9
yopzn <69734173+yopzn@users.noreply.github.com>
2024-03-17 14:47:40 -0500

    Add libraries and basic utils module

---
+++ src/entity/Utils.java
@@ -0,0 +1,21 @@
+package src.entity;
+
+public class Utils {
+    public enum Gate {
+        OR,
+        AND
+    }
+
+    public enum MoleculeType {
+        A,
+        B,
+        C,
+        D,
+        E,
+        R,
+        S,
+        T,
+        U
+    }
+
+}
